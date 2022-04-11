c5e29d480ed416de3ea3b683430b6127db278791
HackaPhil <philip.gyesi-appiah@durham.ac.uk>
2022-04-11 15:29:29 +0100

    start frontend

---
+++ plantToPlate/navigation/intro/welcome/Welcome.styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    background: {
+      backgroundColor: '#32910F',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column'
+    },
+    text: {
+      color: '#fff',
+      fontSize: 22,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: 20,
+      marginTop: 30,
+    },
+    btn: {
+      backgroundColor: '#40B913',
+      borderRadius: 50,
+      marginTop: 100,
+    },
+    btnText: {
+      fontSize: 22,
+      paddingHorizontal: 40,
+      paddingVertical: 15,
+      color: '#fff',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+});
